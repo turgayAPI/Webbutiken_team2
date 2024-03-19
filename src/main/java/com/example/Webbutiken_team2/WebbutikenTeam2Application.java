573bf7bc970a1e39f60727b4d614119f68962668
Turgay S <tsokmen76@gmail.com>
2024-03-19 12:49:01 +0100

    Initial commit

---
+++ src/main/java/com/example/Webbutiken_team2/WebbutikenTeam2Application.java
@@ -0,0 +1,13 @@
+package com.example.Webbutiken_team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbutikenTeam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbutikenTeam2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Webbutiken_team2/WomensClothingStepDef.java
@@ -0,0 +1,4 @@
+package com.example.Webbutiken_team2;
+
+public class WomensClothingStepDef {
+}
